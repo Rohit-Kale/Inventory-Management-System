7c44f1b0441da26dc8b8ae03df1e46ecca475a48
Lenovo <Lenovo@SLAYER>
2022-10-28 23:00:49 +0530

    Add project files.

---
+++ POSales/Alert.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSales
+{
+    public partial class Alert : Form
+    {
+        private Random random;
+        private int tempIndex;
+        MainForm main;
+        public Alert(MainForm mn)
+        {
+            InitializeComponent();
+            random = new Random();
+            Color color = SelectThemeColor();
+            panel1.BackColor = color;
+            main = mn;                        
+        }
+
+        public enum enmAction
+        {
+            wait,
+            start,
+            close
+        }
+        private Alert.enmAction action;
+        private int x, y;
+
+       
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            action = enmAction.close;
+        }
+
+        private void btnReorder_Click(object sender, EventArgs e)
+        {
+            StockIn stock = new StockIn(main);
+            main.openChildForm(stock);
+            ProductStockIn stockIn = new ProductStockIn(stock);
+            stock.ProductForSupplier(lblPcode.Text);
+            stockIn.addStockIn(lblPcode.Text);
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while(tempIndex==index)
+            {
+                //if the color has already been selected, we select again to choose a different one.
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+
+        }
+    }
+}
---
+++ POSales/Recept.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace POSales
+{
+    public partial class Recept : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        SqlDataReader dr;
+        string store;
+        string address;
+        Cashier cashier;
+
+        public Recept(Cashier cash)
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+            cashier = cash;
+            LoadStore();
+        }
+
+        public void LoadStore()
+        {
+            cn.Open();
+            cm = new SqlCommand("SELECT * FROM tbStore", cn);
+            dr = cm.ExecuteReader();
+            dr.Read();
+            if (dr.HasRows)
+            {
+                store = dr["store"].ToString();
+                address = dr["address"].ToString();
+            }
+            dr.Close();
+            cn.Close();
+        }
+
+        private void Recept_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void Recept_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Dispose();
+            }
+        }
+    }
+}
